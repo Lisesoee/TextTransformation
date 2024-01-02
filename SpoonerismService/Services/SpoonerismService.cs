e208b58845eb2745083e009c1f4bb71306a69741
Lise Jacobsen <lise.jacobsen@hansencx.com>
2024-01-02 18:27:12 +0100

    Fix Spoonerism transformation when text doesn't have exactly two words

--- SpoonerismService/Services/SpoonerismService.cs
+++ SpoonerismService/Services/SpoonerismService.cs
@@ -8,16 +8,13 @@
             // Perform spoonerism transformation logic
             string[] words = text.Split(' ');
 
-            // if (words.Length >= 2)
-            // {
-            //     // Swap the initial letter of the first two words
-            //     string temp = words[0].Substring(0, 1);
-            //     words[0] = words[1].Substring(0, 1) + words[0].Substring(1);
-            //     words[1] = temp + words[1].Substring(1);
-            // }
-            string temp = words[0].Substring(0, 1);
+            if (words.Length >= 2)
+            {
+                // Swap the initial letter of the first two words
+                string temp = words[0].Substring(0, 1);
                 words[0] = words[1].Substring(0, 1) + words[0].Substring(1);
                 words[1] = temp + words[1].Substring(1);
+            }
 
             return string.Join(' ', words);
         }
