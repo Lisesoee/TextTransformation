505fa3c40d2069eb673217d3fb108a75a31856af
Lise Jacobsen <lise.jacobsen@hansencx.com>
2024-01-03 12:51:30 +0100

    Add text transformer ui and service

--- TextTransformation/Pages/Index.cshtml.cs
+++ TextTransformation/Pages/Index.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TextTransformation.Services;
 
 namespace TextTransformation.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public string TransformedResult { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -16,5 +18,14 @@ namespace TextTransformation.Pages
         {
 
         }
+        public async Task<IActionResult> OnPostTransform(string text)
+        {
+            var myTextTransformer = new TextTransformerService();
+            TransformedResult = await myTextTransformer.TransformText(text);
+            //TransformedResult = "Wello Horld (simulated)";
+
+            return Page();
+        }
+
     }
 }
---
+++ TextTransformation/Services/TextTransformerService.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+
+namespace TextTransformation.Services
+{
+    public class TextTransformerService
+    {
+         private static RestClient spoonerismRestClient = new RestClient("http://spoonerismmicroservice/SpoonerismService/");
+       
+
+        public async Task<string> TransformText(string text)
+        {
+            var task = spoonerismRestClient.GetAsync<string>(new RestRequest("/Get?text=" + text));
+            await task;
+            return task.Result;
+        }
+    }
+}
